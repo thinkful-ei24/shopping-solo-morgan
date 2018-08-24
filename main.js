821bf6bdb04703bc1e2c08175b8ad4f7fb998640
Morgan Newman <mpnewman95@gmail.com>
2018-08-24 09:17:53 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+// A shopping list should be rendered to the page
+// You should be able to add items to the list
+// You should be able to check items on the list
+// You should be able to delete items from the list
